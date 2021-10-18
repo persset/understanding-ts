25f748943d59f62a860bcb5025597428f50eb527
Jose Victor Persset <jpersset@gmail.com>
2021-10-18 09:25:02 -0300

    understanding union types

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-/*const person: {
-  name: string;
-  age: number;
-} = {*/
-var person = {
-    name: "Jose",
-    age: 24,
-    hobbies: ["Sporrts", "Cooking"]
-};
-var favoriteActivities;
-favoriteActivities = ["Sports"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
