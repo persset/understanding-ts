82cd9d3c7ef3d3b33a1e5d5c1e106289524e8d18
Jose Victor Persset <jpersset@gmail.com>
2021-10-18 10:24:28 -0300

    understanding the 'never' type and wraping up the 'TypeScript Basics & Basic Types' Section

--- app.ts
+++ app.ts
@@ -7,3 +7,11 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): void {
+  throw { message: message, errorCode: code };
+  // while(true) {}
+}
+
+const result = generateError("An error ocurred!", 500);
+console.log(result);
